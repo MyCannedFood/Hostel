7d678651c7cb638f88a28bf7d9b8f06966850d8f
MyCannedFood <sagarawaluya@gmail.com>
2026-05-13 16:55:23 +0700

    add interaction in map

--- resources/views/home/sections/map.blade.php
+++ resources/views/home/sections/map.blade.php
@@ -8,23 +8,29 @@
     <div class="map-container">
         <img src="{{ asset('map-alasare.png') }}" alt="AlaSare Hostel Illustrated Map">
         
-        <!-- <div class="map-legend">
+        <div class="map-hotspots">
+            <a href="#area-hutan" class="hotspot" style="top: 30%; left: 38.5%; width: 10%; height: 5%;" title="Area Hutan"></a>
+            <a href="#the-villas" class="hotspot" style="top: 55.5%; left: 54.5%; width: 10%; height: 5%;" title="The Villas"></a>
+            <a href="#kebun-sayur" class="hotspot" style="top: 64%; left: 69.5%; width: 10%; height: 5%;" title="Kebun Sayur"></a>
+        </div>
+
+        <div class="map-legend">
             <h4>Map Legend</h4>
             <ul class="legend-list">
                 <li class="legend-item">
-                    <span class="legend-color" style="background: #2D4635;"></span>
-                    Pick-In-Brings Points
+                    <span class="legend-color" style="background: #419662;"></span>
+                    Pakis Braga Points
                 </li>
                 <li class="legend-item">
-                    <span class="legend-color" style="background: #C07C4D;"></span>
+                    <span class="legend-color" style="background: #D37D4F;"></span>
                     Living Pharmacy
                 </li>
                 <li class="legend-item">
-                    <span class="legend-color" style="background: #E5E5E5; border: 1px solid #ddd;"></span>
+                    <span class="legend-color" style="background: #fff; border: 1px solid #ddd;"></span>
                     Restored Structures
                 </li>
             </ul>
-        </div> -->
+        </div>
     </div>
 </section>
 
